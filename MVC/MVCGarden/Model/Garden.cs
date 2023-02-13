066e95c192f80dbe56414e0fb92343d6e9b62c09
RayaTakova <r.takova2019@pmg-pernik.com>
2023-02-13 11:08:29 +0200

    add model

---
+++ MVC/MVCGarden/Model/Garden.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCGarden.Model
+{
+    public class Garden
+    {
+        public Garden(decimal vegitablesPrice, decimal fruitsPrice, int vegitablesKg, int fruitsKg)
+        {
+            this.VegitablesPrice = vegitablesPrice;
+            this.FruitsPrice = fruitsPrice;
+            this.VegitablesKg = vegitablesKg;
+            this.FruitsKg = fruitsKg;
+        }
+
+        public decimal VegitablesPrice { get; set; }
+        public decimal FruitsPrice { get; set; }
+        public int VegitablesKg { get; set; }
+        public int FruitsKg { get; set; }
+        public decimal CalculateSum()
+        {
+            return 1.94m * (this.FruitsKg * this.FruitsPrice + this.FruitsPrice + this.VegitablesKg * VegitablesPrice);
+        }
+    }
+}
---
+++ MVC/MVCGarden/Views/Display.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCGarden.Views
+{
+    public class Display
+    {
+        public decimal VegitablesPrice { get; set; }
+        public decimal FruitsPrice { get; set; }
+        public int VegitablesKg { get; set; }
+        public int FruitsKg { get; set; }
+        public decimal Total { get; set; }
+
+    }
+}
