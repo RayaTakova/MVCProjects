5a95b90a63eda15b7d0ef19f5e8517e29ae647f1
RayaTakova <r.takova2019@pmg-pernik.com>
2023-02-13 11:19:04 +0200

    add view

--- MVC/MVCGarden/Views/Display.cs
+++ MVC/MVCGarden/Views/Display.cs
@@ -8,11 +8,32 @@ namespace MVCGarden.Views
 {
     public class Display
     {
+        public Display(decimal vegitablesPrice, decimal fruitsPrice, int vegitablesKg, int fruitsKg, decimal total)
+        {
+            this.VegitablesPrice = vegitablesPrice;
+            this.FruitsPrice = fruitsPrice;
+            this.VegitablesKg = vegitablesKg;
+            this.FruitsKg = fruitsKg;
+            this.Total = total;
+        }
+
         public decimal VegitablesPrice { get; set; }
         public decimal FruitsPrice { get; set; }
         public int VegitablesKg { get; set; }
         public int FruitsKg { get; set; }
         public decimal Total { get; set; }
 
+        public void GetValues()
+        {
+            this.VegitablesPrice = decimal.Parse(Console.ReadLine());
+            this.FruitsPrice = decimal.Parse(Console.ReadLine());
+            this.VegitablesKg = int.Parse(Console.ReadLine());
+            this.FruitsKg = int.Parse(Console.ReadLine());
+        }
+
+        public void ShowResult()
+        {
+            Console.WriteLine(Total);
+        }
     }
 }
